6b249e6f27a1019a83c4f03691d24019954db2ac
Kgadi22 <kg.maraba@gmail.com>
2025-11-08 18:24:05 +0200

    added file

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+const languages = ["C is fun", "Python is cool", "JavaScript is amazing"];
+let output = "";
+
+for (const line of languages) {
+    output += line + "\n";
+}
+
+console.log(output.trim());
